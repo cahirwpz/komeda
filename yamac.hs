10840e88fa172f707d09294eed0f3551648dc234
Krystian Bacławski <krystian.baclawski@gmail.com>
2013-03-24 20:27:13 +0100

    New parser after syntax changes.

--- yamac.hs
+++ yamac.hs
@@ -1,113 +1,347 @@
-{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods -fno-warn-unused-do-bind #-}
 
-import qualified Data.ByteString.Lazy as B
-import Control.Monad.Reader
-import Data.WAVE
-import Data.Int
-import Data.Word
-import Data.Bits
-import Data.Binary
+-- import qualified Data.ByteString.Lazy as B
+-- import Control.Monad.Reader
+import Data.Char (toUpper)
+-- import Data.WAVE
+-- import Data.Word
+-- import Data.Bits
+-- import Data.Binary
+import Data.Ratio
+import GHC.Float (double2Float)
+import qualified Data.String.Utils as Str
 import Text.ParserCombinators.Parsec
 import qualified Text.ParserCombinators.Parsec.Token as Token
 import Text.ParserCombinators.Parsec.Language (javaStyle)
 
 -- abstract tree representation of parsed music file
 
-data Instrument = Sample { samplePath :: String }
-     deriving Show
+type Nat = Int
+type Frac = Ratio Nat
 
-data Command = BPM Int                   -- bit = quarter-notes
-             | Octave Int                -- preselects octave
-             | Select String             -- index in instrument list
-             | Loop Integer [Command]    -- how many times to repeat
-             | Pitch (Maybe Int) String  -- [octave] pitch
-             | Wait Int                  -- in quarter-notes
-             | Play Int                  -- same as above
-     deriving Show
+data Semitone = C | Cs | D | Ds | E | F | Fs | G | Gs | A | As | B
+data NoteLength = NoteLength Frac
+data NotePitch = NotePitch { semitone :: Semitone, octave :: Nat }
+data Note = Note NotePitch NoteLength
+data Playable = Play Note | PlayTie [Note] | Rest NoteLength
 
-data Music = Music { instruments :: [(String, Instrument)],
-                     channels    :: [(Int, [Command])] }
-     deriving Show
+data Setting = SetTempo Nat          -- beats per minute (in quarter-notes)
+             | ModTempo Int          -- modify tempo by bpm
+             | SetUnit Nat           -- default note length (1/n note)
+             | ModPitch Int          -- pitch change by n halftones
+             | SetVolume Float       -- set volume (default value: 1.0)
+             | ModVolume Float       -- modify volume by f
+             | UseVoice String       -- index in instrument list
 
-musicStyle = javaStyle
+data Command = Sequence [Playable]   -- sequence of playable commands
+             | Modify [Setting]      -- setting change
+             | Insert String         -- play pattern of given name
+             | Times Nat [Command]   -- repeat command stream n times
+             | Forever [Command]     -- repeat command stream forever
+             | On Nat [Command]      -- on i-th iteration execute some commands
 
-lexer = Token.makeTokenParser musicStyle
+data Voice = Voice { audioFile    :: String,
+                     defaultPitch :: NotePitch }
+  deriving Show
+
+data Music = Music { voices   :: [(String, Voice)],
+                     patterns :: [(String, [Command])],
+                     channels :: [(Int, [Command])] }
+   deriving Show
+
+-- pretty printers for AST types
+
+show_list l = Str.join " " (map show l) 
+show_diff n = if n >= 0 then "+" ++ show n else show n
+
+instance Show Semitone where
+  show C  = "c"
+  show Cs = "c#"
+  show D  = "d"
+  show Ds = "d#"
+  show E  = "e"
+  show F  = "f"
+  show Fs = "f#"
+  show G  = "g"
+  show Gs = "g#"
+  show A  = "a"
+  show As = "a#"
+  show B  = "b"
+
+instance Show NoteLength where
+  show (NoteLength l) =
+    let n = numerator l
+        d = denominator l
+        in case (n, d) of
+                (1, 1) -> ""
+                (1, _) -> "/" ++ show d
+                (_, 1) -> show n
+                _      -> show n ++ "/" ++ show d
+
+instance Show NotePitch where
+  show (NotePitch semitone' octave') =
+    if octave' <= 3
+      then show semitone' ++ take (3 - octave') (repeat ',')
+      else map toUpper (show semitone') ++ take (octave' - 4) (repeat '\'')
+
+instance Show Note where
+  show (Note pitch' length') = show pitch' ++ show length'
+
+instance Show Playable where
+  show (Play n) = show n
+  show (Rest l) = "R" ++ show l
+  show (PlayTie nl) = "(" ++ show_list nl ++ ")"
+
+instance Show Setting where
+  show (SetTempo n) = "tempo " ++ show n
+  show (ModTempo n) = "tempo " ++ show_diff n
+  show (SetUnit n) = "unit 1/" ++ show n
+  show (ModPitch n) = "pitch " ++ show_diff n
+  show (SetVolume f) = "volume " ++ show f
+  show (ModVolume f) = "volume " ++ show_diff f
+  show (UseVoice s) = "voice " ++ show s
+
+instance Show Command where
+  show (Sequence l) = Str.join ", " (map show l) 
+  show (Modify l) = "[" ++ show_list l ++ "]"
+  show (Insert s) = "{insert " ++ show s ++ "}"
+  show (Times n l) = "{times " ++ show n ++ ": " ++ show_list l ++ "}"
+  show (Forever l) = "{forever: " ++ show_list l ++ "}"
+  show (On n l) = "{on " ++ show n ++ ": " ++ show_list l ++ "}"
+
+-- let's reuse some of Java parser elements and define some parser helpers
+
+yamaStyle = javaStyle
+
+lexer = Token.makeTokenParser yamaStyle
 lexeme = Token.lexeme lexer
 braces = Token.braces lexer
+parens = Token.parens lexer
+brackets = Token.brackets lexer
 natural = Token.natural lexer
-identifier = Token.identifier lexer
+colon = Token.colon lexer
+float = Token.float lexer
 symbol = Token.symbol lexer
+commaSep1 = Token.commaSep1 lexer
 stringLiteral = Token.stringLiteral lexer
 whiteSpace = Token.whiteSpace lexer
 
-instrument = do
-  symbol "Load"
-  filename <- stringLiteral
-  return $ Sample filename
+sign = oneOf "+-"
+tryParser p = option Nothing $ do {x <- p; return $ Just x}
 
-namedInstrument = do
-  symbol "Instrument"
-  name <- stringLiteral
-  inst <- braces instrument
-  return (name, inst)
+-- Playable parsers
 
-cmdBPM = do
+readDenominator = do
+  char '/'
+  den <- natural
+  return den
+
+readNoteLength = do
+  num <- option 1 natural
+  den <- option 1 readDenominator
+  return $ NoteLength (fromIntegral num % fromIntegral den)
+
+readFlatLowSemitone = do
+  t <- oneOf "eb"
+  return $ case t of
+                'e' -> E
+                'b' -> B
+
+readRichLowSemitone = do
+  t <- oneOf "cdfga"
+  s <- tryParser (char '#')
+  let t' = case s of Just '#' -> [t, '#']; _ -> [t]
+      in return $ case t' of
+                       "c"  -> C
+                       "c#" -> Cs
+                       "d"  -> D
+                       "d#" -> Ds
+                       "f"  -> F
+                       "f#" -> Fs
+                       "g"  -> G
+                       "g#" -> Gs
+                       "a"  -> A
+                       "a#" -> As
+
+readFlatHighSemitone = do
+  t <- oneOf "EB"
+  return $ case t of
+                'E' -> E
+                'B' -> B
+
+readRichHighSemitone = do
+  t <- oneOf "CDFGA"
+  s <- tryParser (char '#')
+  let t' = case s of Just '#' -> [t, '#']; _ -> [t]
+      in return $ case t' of
+                       "C"  -> C
+                       "C#" -> Cs
+                       "D"  -> D
+                       "D#" -> Ds
+                       "F"  -> F
+                       "F#" -> Fs
+                       "G"  -> G
+                       "G#" -> Gs
+                       "A"  -> A
+                       "A#" -> As
+
+readLowNote = do
+  t <- readFlatLowSemitone <|> readRichLowSemitone
+  o <- many (char ',')
+  return $ NotePitch t (3 - length o)
+
+readHighNote = do
+  t <- readFlatHighSemitone <|> readRichHighSemitone
+  o <- many (char '\'')
+  return $ NotePitch t (4 + length o)
+
+readNotePitch = readLowNote <|> readHighNote
+
+readNote = do
+  p <- readNotePitch
+  l <- readNoteLength
+  return $ Note p l
+
+readRest = do
+  char 'R'
+  l <- readNoteLength
+  return $ Rest l
+
+readTie = do
+  ns <- parens $ many1 (lexeme readNote)
+  return $ PlayTie ns
+
+readPlayable = readTie <|> readRest <|> do {n <- readNote; return $ Play n}
+
+-- Setting parsers
+
+opt_sign = option Nothing (do { s <- sign; return (Just s); })
+
+readTempo = do
+  modifier <- opt_sign
   beats <- natural
-  return $ BPM (fromIntegral beats)
+  let bpm = fromIntegral beats
+      in return $ case modifier of
+                       Just '+' -> ModTempo bpm
+                       Just '-' -> ModTempo (-bpm)
+                       Nothing  -> SetTempo bpm
 
-cmdOctave = do
-  octave <- natural
-  return $ Octave (fromIntegral octave)
+readVolume = do
+  modifier <- opt_sign
+  volume' <- float
+  let vol = double2Float volume'
+      in return $ case modifier of
+                       Just '+' -> ModVolume vol
+                       Just '-' -> ModVolume (-vol)
+                       Nothing  -> SetVolume vol
 
-cmdSelect = do
+readPitch = do
+  modifier <- sign 
+  semitones <- natural
+  let n = fromIntegral semitones
+      in return $ case modifier of
+                       '+' -> ModPitch n
+                       '-' -> ModPitch (-n)
+
+readUnit = do
+  char '1'
+  char '/'
+  unit <- natural
+  return $ SetUnit (fromIntegral unit)
+
+readVoice = do
   name <- stringLiteral
-  return $ Select name
+  return $ UseVoice name
 
-cmdLoop = do
-  times <- natural
-  cmds <- commandBlock
-  return $ Loop times cmds
+readSetting = do
+  name <- symbol "tempo" <|> (try $ symbol "volume") <|> symbol "pitch" <|> symbol "unit" <|> symbol "voice"
+  case name of
+       "tempo"  -> readTempo
+       "volume" -> readVolume
+       "pitch"  -> readPitch
+       "unit"   -> readUnit
+       "voice"  -> readVoice
 
-cmdPitch = do
-  pitch <- identifier
-  return $ Pitch Nothing pitch
+-- Command parsers
 
-cmdPlay = do
-  notes <- natural
-  return $ Play (fromIntegral notes)
+readSettingsChange = brackets $ do
+  settings <- commaSep1 readSetting
+  return $ Modify settings
 
-cmdWait = do
-  notes <- natural
-  return $ Wait (fromIntegral notes)
+readSequence = do
+  sequence <- many1 (lexeme readPlayable)
+  return $ Sequence sequence
 
-command = do
-  cmd <- identifier
-  case cmd of
-       "BPM" -> cmdBPM
-       "Loop" -> cmdLoop
-       "Octave" -> cmdOctave
-       "Play" -> cmdPlay
-       "Wait" -> cmdWait
-       "Pitch" -> cmdPitch
-       "Select" -> cmdSelect
-       _ -> unexpected $ "command: " ++ cmd
+readTimes = do
+  n <- natural
+  colon
+  cs <- commands
+  return $ Times (fromIntegral n) cs
 
-commandBlock = braces $ many1 (lexeme command)
+readInsert = do
+  pattern <- stringLiteral
+  return $ Insert pattern
 
-channel = do
-  symbol "Channel"
-  number <- natural
-  cmds <- commandBlock
-  return ((fromIntegral number), cmds)
+readForever = do
+  colon
+  cs <- commands
+  return $ Forever cs
+
+readOn = do
+  i <- natural
+  colon
+  cs <- commands
+  return $ On (fromIntegral i) cs
+
+readControlCommand = braces $ do
+  name <- symbol "times" <|> symbol "insert" <|> symbol "forever" <|> symbol "on"
+  case name of
+            "times"   -> readTimes
+            "insert"  -> readInsert
+            "forever" -> readForever
+            "on"      -> readOn
+
+command  = readSettingsChange <|> readSequence <|> readControlCommand
+commands = many1 (lexeme command)
+
+-- Voice parsers
+
+readVoiceBody = do
+  symbol "file"
+  filename <- lexeme stringLiteral
+  symbol "pitch"
+  pitch <- lexeme readNotePitch
+  return $ Voice filename pitch
+
+-- Top-level parsers
+
+readVoiceDef = do
+  symbol "voice"
+  name <- stringLiteral
+  body <- braces readVoiceBody
+  return (name, body)
+
+readPatternDef = do
+  symbol "pattern"
+  name <- stringLiteral
+  cs <- braces commands
+  return (name, cs)
+
+readChannelDef = do
+  symbol "channel"
+  n <- natural
+  cs <- braces commands
+  return (fromIntegral n, cs)
 
 music = do
   whiteSpace
-  instruments <- many1 (lexeme namedInstrument)
-  channels <- many1 (lexeme channel)
-  return $ Music instruments channels
+  vs <- many1 (lexeme readVoiceDef)
+  ps <- many (lexeme readPatternDef)
+  cs <- many1 (lexeme readChannelDef)
+  return $ Music vs ps cs
 
 -- compiler part
-
+{-
 {-
   Binary representation of a command is composed of command byte and
   arbitrary number of following bytes that express arguments.
@@ -261,7 +495,7 @@ readSoundFiles m = do
 assemble m = do
   ss <- readSoundFiles m
   return $ MusicRaw (map Stream $ compileChannels m) ss
-
+-}
 -- main part
 
 main = do
@@ -270,6 +504,6 @@ main = do
     Left e -> do putStrLn "Error parsing input:"
                  print e
     Right m -> do print m
-                  raw <- assemble m
+                  {- raw <- assemble m
                   print raw
-                  encodeFile "test.bin" raw
+                  encodeFile "test.bin" raw -}
