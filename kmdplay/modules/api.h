eef1abe9d68b5e5de338272d2c22a51d7db93058
Krystian Bacławski <krystian.baclawski@gmail.com>
2013-07-04 12:42:40 +0200

    Add commands for interaction with modules.

--- kmdplay/modules/api.h
+++ kmdplay/modules/api.h
@@ -5,8 +5,38 @@
 #include <stdint.h>
 #include <stddef.h>
 
-typedef enum { CMD_NONE, CMD_HALT, CMD_REST, CMD_PLAY } CommandTypeT;
+typedef enum {
+  CMD_CONTINUE, /* continue execution by invoking previous action by calling
+                   either module's function or performing interpreter step */
+  CMD_ERROR,    /* can be returned by both interpreter and module to indicate
+                   that it has experienced unrecoverable error */
+  CMD_HALT,     /* indicates that there'll be no more data produced for this
+                   channel and the machine should halt its execution */ 
+  CMD_REST,     /* rest for certain amount of time */
+  CMD_PLAY,     /* play a note of given pitch for certain amount of time */
 
+  /* types below are valid only in context of interaction with a module */
+
+  CMD_INIT,     /* to be called only at initialization stage (can't block) */
+  CMD_GET,      /* access module's public property (may block) */
+  CMD_SET,      /* modify module's public property (may block) */
+  CMD_CALL,     /* call module's method (may block) */
+  CMD_RETURN,   /* set by module to indicate that control should return to the
+                   interpreter */
+
+  /* channel can wait for one event at a time, we should know who's going to
+   * send a response and its type */
+
+  CMD_WAIT,     /* wait for an event to arrive, possibly with timeout */
+  CMD_NOTIFY    /* send an event to one or more channels, with extra user data
+                   if needed */
+} CommandTypeT;
+
+/* maximum number of parameters module's method can accept */
+#define MAX_PARAMS 8
+
+/* conveys information about command between three components of the system:
+ * module, scheduler and interpreter */
 typedef struct Command {
   CommandTypeT type;
   union {
@@ -17,21 +47,35 @@ typedef struct Command {
       uint8_t n, d;
       uint8_t pitch;
     } play;
+    struct {
+      uint8_t num;
+      uint8_t reg_num;
+      uint8_t value;
+    } property;
+    struct {
+      uint8_t num;
+      uint8_t param[MAX_PARAMS];
+    } method;
+    struct {
+      uint32_t miliseconds;
+    } wait;
+    struct {
+      uint32_t channel_mask;
+      void *userdata;
+    } notify;
   };
 } CommandT;
 
 /* To be defined by each module separately. */
 typedef struct ModuleState ModuleStateT;
 
-/*
- * TODO: all methods may block? if so then we have to design mechanism that
- *       will pass events between module's methods and the scheduler.
- *
- *       Moreover a method can fail, then it returns "false" then which should
- *       stop the machine.
- */
-
-typedef enum { MOD_VOICE, MOD_SENSOR } ModuleTypeT;
+typedef enum {
+  MOD_VOICE,    /* produces sound */
+  MOD_SENSOR,   /* reads values from sensors */
+  MOD_SYNC,     /* implements synchronization mechanism */
+  MOD_COMM,     /* implements communication between devices */
+  MOD_GENERATOR /* generates notes to be played*/
+} ModuleTypeT;
 
 typedef struct ModuleInterface {
   ModuleTypeT type;
